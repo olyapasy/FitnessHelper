11b2bbaa8a6c94161b784e33151c6d2ff8b1a3f5
Vitaly <meloman787@gmail.com>
2018-06-05 20:18:57 +0300

    Refactoring

--- app/src/main/java/com/fitnesshelper/view/fragments/PersonParamsDialogFragment.java
+++ app/src/main/java/com/fitnesshelper/view/fragments/PersonParamsDialogFragment.java
@@ -49,9 +49,7 @@ public class PersonParamsDialogFragment extends AppCompatDialogFragment {
                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        if (ageVal != null) {
-
-                        } else {
+                        if (ageVal == null) {
                             toast.show();
                             showFragment();
                         }
